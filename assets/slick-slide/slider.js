e9803d01569252aac29cabc3aaa514c20348c129
ThaiQuang1307 <quangmath1307@gmail.com>
2023-04-18 14:39:46 +0700

    first commit

---
+++ assets/slick-slide/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".feedback-slide").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+    dots: true,
+  });
+});
